24da52f86aa9cb7d25bde1c1278ae52af379b0ce
Kedus-the-Great <kedus.f@prhsrobotics.com>
2022-11-01 19:23:50 -0400

    Please Work

---
+++ src/main/java/frc/RobotMap.java
@@ -0,0 +1,15 @@
+package frc;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+
+public class RobotMap {
+    public static final int LEFT_FRONT_DRIVE_PORT = 1;
+    public static final int RIGHT_FRONT_DRIVE_PORT = 2;
+    public static final int LEFT_BACK_DRIVE_PORT = 3;
+    public static final int RIGHT_BACK_DIVE_PORT = 4;
+
+    public static WPI_TalonFX leftFrontDiveMotor = new WPI_TalonFX(LEFT_FRONT_DRIVE_PORT);
+    public static WPI_TalonFX rightFrontDriveMotor = new WPI_TalonFX(RIGHT_FRONT_DRIVE_PORT);
+    public static WPI_TalonFX leftBackDriveMotor = new WPI_TalonFX(LEFT_BACK_DRIVE_PORT);
+    public static WPI_TalonFX rightBackDriveMotor = new WPI_TalonFX(RIGHT_BACK_DIVE_PORT);
+}
