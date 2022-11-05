645001e10e4a3bc0befde8d222cc094b1ff4be8b
kedus <kedus.f@prhsrobotics.com>
2022-11-05 10:42:40 -0400

    Fixed

--- src/main/java/frc/robot/commands/JoystickDrive.java
+++ src/main/java/frc/robot/commands/JoystickDrive.java
@@ -9,40 +9,23 @@ public class JoystickDrive extends CommandBase {
     private final DriveSubsystem driveSubsystem;
     private final static XboxController driverController = RobotContainer.driverController;
 
-public JoystickDrive(DriveSubsystem drivetrain) {
-    driveSubsystem = drivetrain;
-    addRequirements(driveSubsystem);
-}
- execute() {
-     double throttle = driverController.getY();
-    double rotate = driverController.getX();
-
-    if ((throttle > 0 && throttle < 0.25) || (throttle < 0 && throttle > -0.25)) {
-        throttle = 0;
-    } else {
-        throttle = throttle;
+    public JoystickDrive(DriveSubsystem drivetrain) {
+        driveSubsystem = drivetrain;
+        addRequirements(driveSubsystem);
     }
 
-    if ((rotate > 0 && rotate < 0.25) || (rotate < 0 && rotate > -0.25)) {
-        rotate = 0;
-    } else {
-    rotate = 2*rotate;
+    public void execute() {
+        double throttle = driverController.getLeftY();
+        double rotate = driverController.getRightX();
+
+        if ((throttle > 0 && throttle < 0.25) || (throttle < 0 && throttle > -0.25)) {
+            throttle = 0;
+        } 
+
+        if ((rotate > 0 && rotate < 0.25) || (rotate < 0 && rotate > -0.25)) {
+            rotate = 0;
+        } 
+
+        driveSubsystem.drive(throttle*0.35, -rotate*0.35);
     }
-
-    if (driverController.getTriggerAxis() > 0.25) {
-        throttle = Math.signum(throttle) * 0.75;
-    }
-    else if (driverController.getAButton()) {
-        throttle = (throttle*1.1);
-    }
-    else {
-        throttle = (throttle*0.8);
-    }
-    driveSubsystem.drive(throttle, rotate);
- }
-
-
-
-
-
 }
